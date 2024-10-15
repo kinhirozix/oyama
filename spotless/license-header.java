2dde219d1c63efa144954f194d262d7cd0dbdfce
kinhirozix <kinhirozix@outlook.com>
2024-10-15 23:28:02 +0800

    Modify license header 2024.10.15 (:

--- spotless/license-header.java
+++ spotless/license-header.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2024 Kinhiro Zix
+ * Copyright 2024 Kinhiro and contibutors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
